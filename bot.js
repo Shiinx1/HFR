6be126396d3455601ce164549fcfdd6dcbe45c0b
AngelMegu <koriio_o@hotmail.com>
2018-08-10 01:13:36 +0200

    aze

--- bot.js
+++ bot.js
@@ -20,6 +20,28 @@ setInterval(GameBar, 30000);
 setInterval(FixEvery5M, 900000);
 })
 
+function adminVI(){
+	Chan1 = client.channels.find("id", "469603172676272148");
+	Chan2 = client.channels.find("id", "469603233514651648");
+	Chan3 = client.channels.find("id", "469603216565207040");
+	Chan4 = client.channels.find("id", "469603840841482252");
+	Chan5 = client.channels.find("id", "469603824705732619");
+	Chan6 = client.channels.find("id", "469603799624056843");
+	Chan7 = client.channels.find("id", "469603247233957893");
+	Chan8 = client.channels.find("id", "469603267828252672");
+	Chan9 = client.channels.find("id", "469603859568918538");
+	Chan10 = client.channels.find("id", "469603894301818881");
+	verifyImages(Chan1);
+	verifyImages(Chan2);
+	verifyImages(Chan3);
+	verifyImages(Chan4);
+	verifyImages(Chan5);
+	verifyImages(Chan6);
+	verifyImages(Chan7);
+	verifyImages(Chan8);
+	verifyImages(Chan9);
+	verifyImages(Chan10);
+}
 function FixEvery5M(){
 	Chan1 = client.channels.find("id", "469603172676272148");
 	Chan2 = client.channels.find("id", "469603233514651648");
@@ -51,6 +73,46 @@ function FixEvery5M(){
 	cleanChannel(Chan8);
 	cleanChannel(Chan9);
 	cleanChannel(Chan10);
+}
+
+function verifyImages(input){
+	input.fetchMessages({ limit: 100 }).then(msgs => {
+				msgs.forEach(function(msg){
+					if(msg.attachments.size > 0){
+				var argsM = msg.createdAt.toString().split(" ");
+				var dateObj = new Date();
+				var n = dateObj.getUTCDate();
+				if(+argsM[2]+7 > 29){
+					var jours = n + ( 29 - +argsM[2]); // JOUR + 7 > 29
+					console.log("Jours au dessus de 29: "+jours);
+				} else {
+					var jours = n - +argsM[2];
+					console.log("Jour en dessous de 29: "+jours); // JOUR + 7 < 29
+				}
+				 if(jours > 2){ // 1 = jour actuel, 2 = jour de l'image
+					 //Check ratings
+					 
+					var NbZero = +msg.reactions.find(reaction => reaction.emoji.id == "469736145224204298").count - 1;
+					var NbVingt = +msg.reactions.find(reaction => reaction.emoji.id == "469736164639637504").count - 1;
+					var NbQuarante = +msg.reactions.find(reaction => reaction.emoji.id == "469736177981718535").count - 1;
+					var NbSoixante = +msg.reactions.find(reaction => reaction.emoji.id == "469736192007471111").count - 1;
+					var NbQuatrevingt = +msg.reactions.find(reaction => reaction.emoji.id == "469736204514885644").count - 1;
+					var NbCent = +msg.reactions.find(reaction => reaction.emoji.id == "469736216494080001").count - 1;
+					var Total = NbZero+NbVingt+NbQuarante+NbSoixante+NbQuatrevingt+NbCent;
+					var TotalValeur = (NbZero)+(NbVingt*20)+(NbQuarante*40)+(NbSoixante*60)+(NbQuatrevingt*80)+(NbCent*100);
+					var Moyenne = TotalValeur/Total;
+					console.log("Moyenne: "+Moyenne);
+					if(Moyenne < 60){
+						msg.delete();
+					}
+					} else {
+						console.log("Pas d'images de plus de 6 jours.");
+					}
+                } else {
+                    msg.delete();
+					}
+				})
+			})
 }
 
 function fixChannel(input){
@@ -104,44 +166,16 @@ client.on('message', (msg) => {
 		}
     if(msg.content == "!vi"){
 		if(msg.member.hasPermission('ADMINISTRATOR')){
-			// verify images
-			msg.channel.fetchMessages({ limit: 100 }).then(msgs => {
-				msgs.forEach(function(msg){
-					if(msg.attachments.size > 0){
-				var argsM = msg.createdAt.toString().split(" ");
-				var dateObj = new Date();
-				var n = dateObj.getUTCDate();
-				if(+argsM[2]+7 > 29){
-					var jours = n + ( 29 - +argsM[2]); // JOUR + 7 > 29
-					console.log("Jours au dessus de 29: "+jours);
-				} else {
-					var jours = n - +argsM[2];
-					console.log("Jour en dessous de 29: "+jours); // JOUR + 7 < 29
-				}
-				 if(jours > 6){ // 1 = jour actuel, 2 = jour de l'image
-					 //Check ratings
-					 
-					var NbZero = +msg.reactions.find(reaction => reaction.emoji.id == "469736145224204298").count - 1;
-					var NbVingt = +msg.reactions.find(reaction => reaction.emoji.id == "469736164639637504").count - 1;
-					var NbQuarante = +msg.reactions.find(reaction => reaction.emoji.id == "469736177981718535").count - 1;
-					var NbSoixante = +msg.reactions.find(reaction => reaction.emoji.id == "469736192007471111").count - 1;
-					var NbQuatrevingt = +msg.reactions.find(reaction => reaction.emoji.id == "469736204514885644").count - 1;
-					var NbCent = +msg.reactions.find(reaction => reaction.emoji.id == "469736216494080001").count - 1;
-					var Total = NbZero+NbVingt+NbQuarante+NbSoixante+NbQuatrevingt+NbCent;
-					var TotalValeur = (NbZero)+(NbVingt*20)+(NbQuarante*40)+(NbSoixante*60)+(NbQuatrevingt*80)+(NbCent*100);
-					var Moyenne = TotalValeur/Total;
-					console.log("Moyenne: "+Moyenne);
-					if(Moyenne < 60){
-						msg.delete();
-					}
-					} else {
-						console.log("Pas d'images de plus de 6 jours.");
-					}
-                } else {
-                    msg.delete();
-					}
-				})
-			})
+			// verify images single channel
+			verifyImages(msg.channel);
+		} else {
+			msg.channel.send("You don't have the permission to execute that command!");
+			}
+		}
+		if(msg.content == "!adminvi"){
+		if(msg.member.hasPermission('ADMINISTRATOR')){
+			// admin verify all channels
+			adminVI();
 		} else {
 			msg.channel.send("You don't have the permission to execute that command!");
 			}
